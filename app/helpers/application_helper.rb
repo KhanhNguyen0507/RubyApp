f311af829fd4573c523f010f7b468a88512b3d07
KhanhNguyen0507 <0712209@gmail.com>
2015-12-26 21:40:46 +0700

    link to pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+	def gravatar_for(shop, options = {size: 80})
+		gravatar_id = Digest::MD5::hexdigest(shop.email.downcase)
+		size = options[:size]
+		gravatar_url = "https://s.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+		image_tag(gravatar_url, alt: shop.shopname, class:"gravatar")
+	end
 end
