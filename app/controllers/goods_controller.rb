bf72c85f94e2ad8fbb41a31454c78813fe6eabb3
KhanhNguyen0507 <0712209@gmail.com>
2015-12-27 14:37:08 +0700

    Create new Create Good form

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -6,4 +6,25 @@ class GoodsController < ApplicationController
   def show
   	@good = Good.find(params[:id])
   end
+
+  def new
+  	@good = Good.new
+  end
+
+  def create
+  	@good = Good.new(good_params)
+  	@good.shop = Shop.find(2)
+
+  	if @good.save
+  		flash[:success] = "Your Good is created successfully!"
+  		redirect_to goods_path
+  	else
+  		render :new
+  	end
+  end
+
+  private
+  def good_params
+  	params.require(:good).permit(:name, :price, :summary, :description)
+  end
 end
